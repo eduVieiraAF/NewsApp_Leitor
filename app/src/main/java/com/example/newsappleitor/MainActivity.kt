0c5c21ce0461ccedf4b75077577de9f7bd906a0f
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-26 15:35:12 -0300

    Layout

--- app/src/main/java/com/example/newsappleitor/MainActivity.kt
+++ app/src/main/java/com/example/newsappleitor/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.newsappleitor
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newsappleitor.databinding.ActivityMainBinding
+import com.example.newsappleitor.databinding.ArtigoBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
 
@@ -14,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
+        val button: Button = binding.btnClick
+        button.setOnClickListener {
+            setContentView(ArtigoBinding.inflate(layoutInflater).root)
+        }
+    }
 }
