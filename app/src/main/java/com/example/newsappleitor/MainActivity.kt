412830682256eb157c7ac2dd78a674ee84af3c53
Eduardo Vieira <edu_vieira@hotmail.com>
2022-11-25 21:57:15 -0300

    Configurando e puxando os dados do firestore(ok)

--- app/src/main/java/com/example/newsappleitor/MainActivity.kt
+++ app/src/main/java/com/example/newsappleitor/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.newsappleitor
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+    val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val notícias = db.collection("notícias")
+        .get()
+        .addOnSuccessListener { res ->
+            for (document in res) {
+                Log.d("NoticiaSucc", "${document}")
+            }
+        }
+
+        .addOnFailureListener { e ->
+            Log.d("NoticiaFail", "NOT FOUND")
+
+        }
 }
